b147545850a84bf4a8233302e0a4a1bef5e420c7
Dan Wolfe <dwolfe2@nd.edu>
2020-12-09 16:08:48 -0500

    Actual site init

---
+++ content/configuration.js
@@ -0,0 +1,14 @@
+const languages = {
+  default: 'en',
+  allowed: ['en'],
+}
+
+module.exports = {
+  siteMetadata: {
+    title: 'Inquisitio',
+    author: 'University of Notre Dame Hesburgh Libraries Rare Books & Special Collections',
+    description: 'Manuscript and print sources for the study of Inquisition history.',
+    siteUrl: 'https://inquisition.library.nd.edu/',
+    languages: languages,
+  },
+}
