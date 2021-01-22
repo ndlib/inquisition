65953a18570e2ce71688cb6fca31d6326d9827fa
Jon Hartzler <jon.hartzler@gmail.com>
2021-01-22 09:48:41 -0500

    disallow

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
           },
           production: {
             policy: [
-              { userAgent: '*', allow: '/' },
+              { userAgent: '*', disallow: ['/'] },
             ],
           },
         },
