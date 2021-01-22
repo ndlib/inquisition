111e9424d5bd306435f6026b1213338124f89156
Jon Hartzler <jon.hartzler@gmail.com>
2021-01-22 09:43:16 -0500

    update to get inquisitions to build to s3 plus a few config things.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,49 @@
 const configuration = require('./content/configuration')
+const s3BucketName = process.env.S3_DEST_BUCKET || ''
+
 module.exports = {
   siteMetadata: configuration.siteMetadata,
   plugins: [
-    '@ndlib/gatsby-theme-marble',
+    {
+      resolve: '@ndlib/gatsby-theme-marble',
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: configuration.siteMetadata.siteUrl,
+        sitemap: `${configuration.siteMetadata.siteUrl}/sitemap.xml`,
+        env: {
+          development: {
+            policy: [
+              { userAgent: '*', disallow: ['/'] },
+            ],
+          },
+          production: {
+            policy: [
+              { userAgent: '*', allow: '/' },
+            ],
+          },
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: configuration.manifest,
+    },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: s3BucketName,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-preconnect',
+      options: {
+        domains: [
+          'https://static.nd.edu',
+          'https://image-iiif.library.nd.edu',
+        ],
+      },
+    },
   ],
 }
