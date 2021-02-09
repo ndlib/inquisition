ca73ef15239428e58107b9d61d5d13b35fc3d3d8
Jon Hartzler <jon.hartzler@gmail.com>
2021-02-09 13:52:01 -0500

    add theme file for inqui

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,7 @@
+import merge from 'lodash.merge'
+import { theme } from '@ndlib/gatsby-theme-marble/src/gatsby-plugin-theme-ui'
+export default merge({}, theme, {
+  colors: {
+    primary: '#ae9142',
+  },
+})
