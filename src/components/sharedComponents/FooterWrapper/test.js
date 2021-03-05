253797354bfd166c3f06e3b275e48085a3950c60
Jon Hartzler <jon.hartzler@gmail.com>
2021-03-05 12:40:25 -0500

    fix test

--- src/components/sharedComponents/FooterWrapper/test.js
+++ src/components/sharedComponents/FooterWrapper/test.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { useStaticQuery } from 'gatsby'
 import Footer from './'
 import Menu from '@ndlib/gatsby-theme-marble/src/components/Shared/Menu'
 
+const sq = {
+  menusJson: {
+  },
+}
+
 test('Footer renders some divs with markdown text', () => {
+  useStaticQuery.mockImplementationOnce(() => {
+    return sq
+  })
   const wrapper = shallow(<Footer location={{ location: '' }}><div id='test'>Children</div></Footer>)
 
   // test the children.
   //
-  expect(wrapper.find(Menu).props().menu).toEqual('footer')
+  expect(wrapper.find(Menu).props().variant).toEqual('footer')
 })
