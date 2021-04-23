d2794ed585091769d4f02276f91730d48f3bfd8a
Jon Hartzler <jon.hartzler@gmail.com>
2021-04-23 12:08:29 -0400

    quick browse update

--- src/pages/search.js
+++ src/pages/search.js
@@ -10,63 +10,64 @@ import SearchRefinementListFilter from 'components/Shared/SearchTools/SearchRefi
 import { TagFilterConfig } from 'searchkit'
 import NDBrandLayout from '../components/sharedComponents/NDBrandLayout'
 import NDBrandSection from '../components/sharedComponents/NDBrandSection'
+import NDBrandTitleHeader from '../components/sharedComponents/NDBrandTitleHeader'
 
 import Seo from '@ndlib/gatsby-theme-marble/src/components/Shared/Seo'
 
 const Search = ({ location }) => {
   return (
-    <NDBrandLayout location={location} hideSearch>
-      <Seo
-        data={{}}
-        location={location}
-        title='Search'
-      />
-      <NDBrandSection variant='fullBleed'>
-        <SearchBase>
-          <SearchFilterBox />
-          <Grid columns={[2, '25% 75%']}>
-            <Box>
-              <TagFilterConfig field='creator.keyword' title='Creator' id='creator' />
-              <TagFilterConfig field='collection.keyword' title='Collection' id='collection' />
+    <SearchBase>
+      <NDBrandLayout location={location} pageHeader={<NDBrandTitleHeader location={location} title='Browse Collection' />}>
+        <Seo
+          data={{}}
+          location={location}
+          title='Search'
+        />
+        <Grid sx={{ ml: '5vw', mr: '5vw' }} columns={['100% 0%', '22vw 68vw', '22vw 68vw']}>
+          <Box sx={{ mt: '5rem' }}>
+            <TagFilterConfig field='creator.keyword' title='Creator' id='creator' />
+            <TagFilterConfig field='collection.keyword' title='Collection' id='collection' />
 
-              <SearchRefinementListFilter
-                field='parent.keyword'
-                label='Document Category'
-                operator='OR'
-                sort='a-z'
-              />
-              <SearchRefinementListFilter
-                field='centuryTag.keyword'
-                label='Date Range'
-                operator='OR'
-                sort='a-z'
-              />
-              <SearchRefinementListFilter
-                field='formatTag.keyword'
-                label='Format'
-                operator='OR'
-              />
-              <SearchRefinementListFilter
-                field='language.keyword'
-                label='Language'
-                operator='OR'
-                sort='default'
-                size='4'
-              />
-              <SearchRefinementListFilter
-                field='geographicLocation.keyword'
-                label='Geogrpahic Location'
-                operator='OR'
-                sort='default'
-              />
-            </Box>
-            <Box>
-              <SearchResults defaultDisplay='list' />
-            </Box>
-          </Grid>
-        </SearchBase>
-      </NDBrandSection>
-    </NDBrandLayout>
+            <SearchRefinementListFilter
+              field='parent.keyword'
+              label='Document Category'
+              operator='OR'
+              sort='a-z'
+            />
+            <SearchRefinementListFilter
+              field='centuryTag.keyword'
+              label='Date Range'
+              operator='OR'
+              sort='a-z'
+            />
+            <SearchRefinementListFilter
+              field='formatTag.keyword'
+              label='Format'
+              operator='OR'
+            />
+            <SearchRefinementListFilter
+              field='language.keyword'
+              label='Language'
+              operator='OR'
+              sort='default'
+              size='4'
+            />
+            <SearchRefinementListFilter
+              field='geographicLocation.keyword'
+              label='Geogrpahic Location'
+              operator='OR'
+              sort='default'
+            />
+          </Box>
+          <NDBrandSection variant='fullBleed'>
+            <SearchFilterBox />
+
+            <SearchResults defaultDisplay='list' />
+          </NDBrandSection>
+        </Grid>
+      </NDBrandLayout>
+    </SearchBase>
+
   )
 }
 
