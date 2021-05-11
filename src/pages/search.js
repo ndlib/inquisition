58a12a6f194bb082103d35c7d128c4675ab3c498
Jon Hartzler <jon.hartzler@gmail.com>
2021-05-11 11:26:04 -0400

    update search

--- src/pages/search.js
+++ src/pages/search.js
@@ -8,6 +8,8 @@ import SearchFilterBox from 'components/Shared/SearchTools/SearchFilterBox'
 import SearchResults from 'components/Shared/SearchTools/SearchResults'
 import SearchRefinementListFilter from 'components/Shared/SearchTools/SearchRefinementListFilter'
 import { TagFilterConfig } from 'searchkit'
+import i18next from '@ndlib/gatsby-theme-marble/src/i18n'
+import { I18nextProvider } from 'react-i18next'
 import NDBrandLayout from '../components/sharedComponents/NDBrandLayout'
 import NDBrandSection from '../components/sharedComponents/NDBrandSection'
 import NDBrandEmptyPageHeader from '../components/sharedComponents/NDBrandEmptyPageHeader'
@@ -18,61 +20,67 @@ import Seo from '@ndlib/gatsby-theme-marble/src/components/Shared/Seo'
 const Search = ({ location }) => {
   return (
     <SearchBase>
-      <NDBrandLayout location={location} pageHeader={<NDBrandEmptyPageHeader location={location} />}>
-        <Seo
-          data={{}}
-          location={location}
-          title='Search'
-        />
-        <Grid sx={{ ml: '5vw', mr: '5vw' }} columns={['100% 0%', '22vw 68vw', '22vw 68vw']}>
-          <Box sx={{ mt: '5rem' }}>
-            <TagFilterConfig field='creator.keyword' title='Creator' id='creator' />
-            <TagFilterConfig field='collection.keyword' title='Collection' id='collection' />
+      <I18nextProvider i18n={i18next}>
+        <NDBrandLayout location={location} pageHeader={<NDBrandEmptyPageHeader location={location} />}>
+          <Seo
+            data={{}}
+            location={location}
+            title='Search'
+          />
+          <Grid sx={{ ml: '5vw', mr: '5vw' }} columns={['100% 0%', '22vw 68vw', '22vw 68vw']}>
+            <Box sx={{ mt: '5rem' }}>
+              <TagFilterConfig field='creator.keyword' title='Creator' id='creator' />
+              <TagFilterConfig field='collection.keyword' title='Collection' id='collection' />
 
-            <SearchRefinementListFilter
-              field='parent.keyword'
-              label='Document Category'
-              operator='OR'
-              sort='a-z'
-            />
-            <SearchRefinementListFilter
-              field='centuryTag.keyword'
-              label='Date Range'
-              operator='OR'
-              sort='a-z'
-            />
-            <SearchRefinementListFilter
-              field='formatTag.keyword'
-              label='Format'
-              operator='OR'
-            />
-            <SearchRefinementListFilter
-              field='language.keyword'
-              label='Language'
-              operator='OR'
-              sort='default'
-              size='4'
-            />
-            <SearchRefinementListFilter
-              field='geographicLocation.keyword'
-              label='Geogrpahic Location'
-              operator='OR'
-              sort='default'
-            />
-          </Box>
-          <NDBrandSection>
-            <NDBrandBreadcrumbs
-              currentPageTitle='Browse Collection'
-              breadcrumbs={[]}
-            />
-            <Heading as='h1' variant='pageTitle'>Browse Collection</Heading>
-
-            <SearchFilterBox />
-
-            <SearchResults defaultDisplay='list' />
-          </NDBrandSection>
-        </Grid>
-      </NDBrandLayout>
+              <SearchRefinementListFilter
+                field='parent.keyword'
+                label='Document Category'
+                operator='OR'
+                sort='a-z'
+              />
+              <SearchRefinementListFilter
+                field='centuryTag.keyword'
+                label='Date Range'
+                operator='OR'
+                sort='a-z'
+              />
+              <SearchRefinementListFilter
+                field='formatTag.keyword'
+                label='Format'
+                operator='OR'
+              />
+              <SearchRefinementListFilter
+                field='language.keyword'
+                label='Language'
+                operator='OR'
+                sort='default'
+                size='4'
+              />
+              <SearchRefinementListFilter
+                field='geographicLocation.keyword'
+                label='Geogrpahic Location'
+                operator='OR'
+                sort='default'
+              />
+            </Box>
+            <NDBrandSection sx={{ pl: '2rem', minWidth: '60vw', '&.sectionContent': { minWidth: '60vw' } }}>
+              <NDBrandBreadcrumbs
+                currentPageTitle='Browse Collection'
+                breadcrumbs={[]}
+              />
+              <div sx={{
+                '& form': {
+                  border: '1px black solid',
+                  borderRadius: '10px',
+                },
+              }}>
+                <SearchFilterBox />
+              </div>
+              <SearchResults defaultDisplay='list' />
+            </NDBrandSection>
+          </Grid>
+        </NDBrandLayout>
+      </I18nextProvider>
     </SearchBase>
 
   )
